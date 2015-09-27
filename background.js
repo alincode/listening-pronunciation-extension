63e3914614e654d89f0f4b8eaa0dd7bae54a04c2
alincode <alin.code@gmail.com>
2015-09-27 18:54:07 +0800

    compatible with English and Chinese directory

--- background.js
+++ background.js
@@ -5,11 +5,11 @@ chrome.commands.onCommand.addListener(function(command){
 	// TODO: improve chrome.tabs.executescript passing parameters
 	if (command == "pron-uk") {
 		chrome.tabs.executeScript({
-    		code: 'document.getElementsByClassName("pron-uk")[0].click()'	
+    		code: 'document.getElementsByClassName("sound")[1].click()'	
   		});
 	}else if (command == "pron-us") {
 		chrome.tabs.executeScript({
-    		code: 'document.getElementsByClassName("pron-us")[0].click()'	
+    		code: 'document.getElementsByClassName("sound")[0].click()'	
   		});
 	}else{
 		console.log(command);
